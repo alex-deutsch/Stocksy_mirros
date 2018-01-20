0bf77302fed57db3277314ef900ecdaa20ce1c66
Alexander Deutsch <mail@alexanderdeutsch.de>
2018-01-20 07:46:03 +0100

    Switch to IEXTrading API

--- frontend/script.js
+++ frontend/script.js
@@ -4,40 +4,34 @@ $(document).ready(function () {
 
 function reloadStocks() {
 
-	var url = "https://f1re.de/stocksy/getQuote.php";
-	var apikey = "";
+	var url = "https://api.iextrading.com/1.0/stock/market/batch";
 	var symbols = "<?php echo getConfigValue('stocksy_stocks'); ?>";
-	var fields 	= "fiftyTwoWkHigh,fiftyTwoWkHighDate,fiftyTwoWkLow,fiftyTwoWkLowDate";
-    var mode  	= "I";
-	var jerq 	= "false";
-	var queryURLQuotes = url + "?apikey=" + apikey +"&symbols=" + symbols + "&fields=" + fields + "&mode=" + mode + "&jerq=" + jerq;
+	var types 	= "quote,news";
+	var last 	= "5";
+	var queryURLQuotes = url + "?symbols=" + symbols + "&types=" + types + "&last=" + last;
 	
-		$.get(queryURLQuotes).done(function(data){
-			$("#stocks_station_table").empty();
-					var i = 0;
-					var json = data.results;
-						$.each(json, function() {
-						if (i < 7){
-							$("#stocks_station_table").append("<tr></tr>");
+	$.get(queryURLQuotes).done(function(data){
+		$("#stocks_station_table").empty();
+				var i = 0;
+				for(key in data){
+					if (i < 7){
+						$("#stocks_station_table").append("<tr></tr>");
+						var stock = data[key]
+						var quote = stock.quote
+						var stockname = quote.companyName;
+						var stockCurrentPrice = quote.iexRealtimePrice;
+						var changeInPercent = quote.changePercent + "%";
 
-							var stockname = this.name;
-							var stockCurrentPrice = this.lastPrice;
-							var changeInPercent = this.percentChange + "%";
-							var currency = this.unitCode;
+						var priceString = stockCurrentPrice + " $";
 
-							var priceString = stockCurrentPrice;
-							if (currency != null && currency == 2) {
-								priceString = priceString + " $";
-							}
+						$("#stocks_station_table tr:last").append("<td>" + stockname + "</td>");
+						$("#stocks_station_table tr:last").append("<td>" + priceString + "</td>");
+						$("#stocks_station_table tr:last").append("<td>" + changeInPercent + "</td>");
 
-							$("#stocks_station_table tr:last").append("<td>" + stockname + "</td>");
-							$("#stocks_station_table tr:last").append("<td>" + priceString + "</td>");
-							$("#stocks_station_table tr:last").append("<td>" + changeInPercent + "</td>");
-
-						}
-						i++;
-					});
-		});
+					}
+					i++;
+				}
+	});
 
 		// reload every 10 minutes
 		window.setTimeout(function() {
